d58b156ee68d575acd1956b22384d3f7b5058b42
BUMBAIYA <amitchauhan263871@gmail.com>
2023-06-04 14:55:14 +0530

    setup and cleanup

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
