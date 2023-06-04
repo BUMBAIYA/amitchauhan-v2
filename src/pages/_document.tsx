48d3c125159b0eb9e4d66157c97c5d76af5e665e
BUMBAIYA <amitchauhan263871@gmail.com>
2023-06-04 16:28:42 +0530

    basic theme setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-zinc-100 text-zinc-900 antialiased dark:bg-zinc-900 dark:text-white">
         <Main />
         <NextScript />
       </body>
