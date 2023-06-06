58834ab0c5e1e7806c140758b06bfee336063512
BUMBAIYA <amitchauhan263871@gmail.com>
2023-06-06 12:29:28 +0530

    added framer motion animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import MainLayout from "@/components/layout/MainLayout";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import MainLayout from "@/components/layout/MainLayout";
+import { AnimatePresence } from "framer-motion";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <AnimatePresence mode="wait" initial={false}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </AnimatePresence>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { PageSEO } from "@/components/PageSEO";
+import FadeUp from "@/components/animation/FadeUp";
 import { cursorTrail } from "@/components/utility/cursorTrail";
+import { AnimatePresence } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 export default function Home() {
@@ -21,13 +23,19 @@ export default function Home() {
       ></canvas>
       <div className="mx-auto max-w-5xl">
         <div className="max-w-3xl">
-          <h1 className="bg-gradient-to-r from-teal-600 to-tera-500 bg-clip-text text-5xl font-bold text-transparent sm:text-6xl md:text-7xl">
-            Creative frontend developer
-          </h1>
-          <p className="mt-8 text-lg font-semibold text-zinc-600 md:text-xl">
-            I&apos;m Amit Chauhan, a frontend developer based in Mumbai, India.
-            I craft captivative user intefaces and designs.
-          </p>
+          <AnimatePresence>
+            <FadeUp key="title-main" duration={0.6}>
+              <h1 className="bg-gradient-to-r from-teal-500 to-tera-500 bg-clip-text text-5xl font-bold text-transparent sm:text-6xl md:text-7xl">
+                Creative frontend developer
+              </h1>
+            </FadeUp>
+            <FadeUp key="description" duration={0.6} delay={0.2}>
+              <p className="mt-8 text-lg font-semibold text-zinc-600 md:text-xl">
+                I&apos;m Amit Chauhan, a frontend developer based in Mumbai,
+                India. I craft captivative user intefaces and designs.
+              </p>
+            </FadeUp>
+          </AnimatePresence>
         </div>
       </div>
     </>
