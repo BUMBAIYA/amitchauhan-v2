d7834cacc19d14d93b8a9e6a2f58c59cef2f018a
BUMBAIYA <amitchauhan263871@gmail.com>
2023-07-08 00:47:08 +0530

    added app analytics from vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,20 @@ import { AnimatePresence } from "framer-motion";
 import MainLayout from "@/components/layout/MainLayout";
 import "@/styles/globals.css";
 import { useRouter } from "next/router";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      <MainLayout>
-        <AnimatePresence mode="wait" initial={false}>
-          <Component key={router.asPath} {...pageProps} />
-        </AnimatePresence>
-      </MainLayout>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <MainLayout>
+          <AnimatePresence mode="wait" initial={false}>
+            <Component key={router.asPath} {...pageProps} />
+          </AnimatePresence>
+        </MainLayout>
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 }
