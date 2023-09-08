98baa643ed1d3e59c1e21d80465101cf33828cc8
Amit Chauhan <amitchauhan263871@gmail.com>
2023-09-08 12:16:42 +0530

    updated hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,13 +32,16 @@ export default function Hero() {
       ref={ref}
       className="pointer-events-none flex h-[calc(100vh-112px)] items-center px-6 sm:px-14 md:px-20"
     >
-      <div className="mx-auto -mt-[112px] max-w-7xl">
-        <div className="max-w-7xl">
+      <div className="-mt-[112px] w-full">
+        <div className="mx-auto max-w-7xl">
           <AnimatePresence>
             <FadeUp key="title-main" duration={0.6}>
-              <h1 className="mx-auto bg-gradient-to-r from-teal-500 to-tera-500 bg-clip-text text-5xl font-bold text-transparent dark:from-teal-200 dark:to-teal-500 sm:text-6xl md:text-7xl xl:text-8xl">
-                React.js application developer
+              <h1 className="bg-gradient-to-r from-teal-500 to-tera-500 bg-clip-text py-2 text-5xl font-bold text-transparent dark:from-teal-200 dark:to-teal-500 sm:text-6xl md:text-7xl xl:text-8xl">
+                Amit Chauhan
               </h1>
+              <span className="text-xl font-semibold text-zinc-900 dark:text-zinc-100 md:text-3xl">
+                Frontend and React.js developer
+              </span>
             </FadeUp>
             <FadeUp key="description" duration={0.6} delay={0.2}>
               <div className="mt-8 max-w-3xl text-base font-semibold text-zinc-900 dark:text-zinc-200 sm:text-base md:text-2xl">
