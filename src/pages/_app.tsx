c9acf9072401eb0352b38e278e1f240797ceb0b6
BUMBAIYA <amitchauhan263871@gmail.com>
2023-12-17 01:57:17 +0530

    added speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import MainLayout from "@/layout/MainLayout";
 import "@/styles/globals.css";
 import { useRouter } from "next/router";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </MainLayout>
       </ThemeProvider>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
