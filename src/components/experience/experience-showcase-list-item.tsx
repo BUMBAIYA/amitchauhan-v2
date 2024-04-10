61896790e000a38cdb4ba80e47b02ad54823d916
BUMBAIYA <amitchauhan263871@gmail.com>
2024-04-11 00:36:07 +0530

    [Update]: rel nofollow for experience company name

--- src/components/experience/experience-showcase-list-item.tsx
+++ src/components/experience/experience-showcase-list-item.tsx
@@ -69,6 +69,7 @@ export default function ExperienceShowcaseListItem(
             href={props.organisation.href}
             className="cursor-pointer text-accent"
             target="_blank"
+            rel="nofollow"
           >
             @{props.organisation.name}
           </Link>
