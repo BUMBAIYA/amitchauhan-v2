70d37c6679ff9e1cba40b8ffd09f9883d174b3dd
BUMBAIYA <amitchauhan263871@gmail.com>
2024-08-02 11:10:45 +0530

    [moved]: analytics from next to react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "next-themes";
 import { AnimatePresence } from "framer-motion";
 
@@ -11,13 +11,15 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      <MainLayout>
-        <AnimatePresence mode="wait" initial={false}>
-          <Component key={router.asPath} {...pageProps} />
-        </AnimatePresence>
-      </MainLayout>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <MainLayout>
+          <AnimatePresence mode="wait" initial={false}>
+            <Component key={router.asPath} {...pageProps} />
+          </AnimatePresence>
+        </MainLayout>
+      </ThemeProvider>
       <Analytics />
-    </ThemeProvider>
+    </>
   );
 }
